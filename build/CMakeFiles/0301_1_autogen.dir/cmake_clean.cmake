f315667f5bba9cd224fcb0f987046e8f0de69c34
hsinying-tsai <a0981962735@gmail.com>
2024-05-31 15:42:35 +0800

    0531

--- build/CMakeFiles/0301_1_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "0301_1_autogen/mocs_compilation.cpp"
-  "CMakeFiles/0301_1_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/0301_1_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
